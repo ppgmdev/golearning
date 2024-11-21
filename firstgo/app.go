7e7d21a6f3f7fda8d8421328f049fe12a44068f3
ppgmdev <ptono98@gmail.com>
2024-11-21 01:21:50 +0000

    first commit

---
+++ firstgo/app.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 6.5
+	var initialInvestment float64
+	var years float64
+	var interestRate float64
+
+	//fmt.Print("Investment Amount: ")
+	outputText("Investrent Amount: ")
+	fmt.Scan(&initialInvestment)
+
+	//fmt.Print("Expected return rate: ")
+	outputText("Expected return rate: ")
+	fmt.Scan(&interestRate)
+
+	//fmt.Print("Years: ")
+	outputText("Years: ")
+	fmt.Scan(&years)
+
+	gains := initialInvestment * math.Pow(1+interestRate/100, years)
+	futureRealValue := gains / math.Pow(1+inflationRate/100, years)
+
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", gains)
+	formattedRFV := fmt.Sprintf(`Future Value Adjusted 
+
+	for inflation: %.1f\n`, futureRealValue)
+	//outputs information
+	//fmt.Println("Future Value:", gains)
+	//fmt.Printf("Future Value: %.1f\n", gains)
+	//fmt.Println("Future Value Adjusted for inflation:", futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
---
+++ firstgo/go.mod
@@ -0,0 +1,3 @@
+module example.com/firstgo
+
+go 1.21.1
