aac0e513bab7adfcc77befb86b52e30b69ee4603
ppgmdev <ptono98@gmail.com>
2024-12-05 23:47:07 +0000

    recursion and slices as params

--- functions-divedeep/main.go
+++ functions-divedeep/main.go
@@ -3,13 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fact := factorial(5)
-	fmt.Println(fact)
+	numbers := []int{1, 10, 15}
+	sum := sumup(1, 2, 10, 16, 16) // starting value of 1
+	anotherSum := sumup(1, numbers...)
+
+	fmt.Println(sum)
+	fmt.Println(anotherSum)
 }
 
-func factorial(number int) int {
-	if number == 0 {
-		return 1
+func sumup(startingValue int, numbers ...int) int { // slice with intents with as many parameters as i want
+	sum := 0
+	fmt.Println("Starting value:", startingValue)
+	for _, val := range numbers {
+		sum += val
 	}
-	return number * factorial(number-1)
+
+	return sum
 }
