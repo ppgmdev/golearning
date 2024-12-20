cc7b3b2f8a7f78c1cf0dddbe7e5a571e507b10b9
ppgmdev <ptono98@gmail.com>
2024-12-20 18:43:24 +0000

    auth events

--- restapi/routes/events.go
+++ restapi/routes/events.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"restapi/models"
+	"restapi/utils"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -35,16 +36,29 @@ func getEvents(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
 		return
 	}
 
-	event.ID = 1
-	event.UserID = 1
+	event.UserID = userId
 
 	err = event.Save()
 	if err != nil {
--- restapi/utils/jwt.go
+++ restapi/utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,4 +17,38 @@ func GenerateToken(email string, userId int64) (string, error) {
 	}) // generate token with data
 
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) (int64, error) {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC) // type assertion
+
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return 0, errors.New("could not parse token")
+	}
+
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return 0, errors.New("invalid token")
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return 0, errors.New("invalid token claims")
+	}
+
+	//email := claims["email"].(string)
+	userId := int64(claims["userId"].(float64))
+
+	return userId, nil
+
 }
